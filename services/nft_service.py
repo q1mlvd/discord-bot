bbf1bac40217850f20b25e5f4828e69d84dbb1c3
q1mlvd <mishakravhuk@gmail.com>
2025-09-30 20:58:34 +0200

    Update nft_service.py

--- services/nft_service.py
+++ services/nft_service.py
@@ -1,30 +1,31 @@
-import discord
 import random
-from PIL import Image
-import io
 
 class NFTService:
-    def __init__(self, bot):
-        self.bot = bot
+    def __init__(self):
         self.nft_collection = {}
+        self.next_id = 1
     
     async def generate_nft(self, user_id: int):
-        """🎨 Генерация уникального NFT"""
-        # Случайные характеристики
-        # Редкость и ценность
-        # Визуальное представление
-        pass
+        """Генерация NFT для пользователя"""
+        nft_id = self.next_id
+        self.next_id += 1
+        
+        rarities = ["Common", "Rare", "Epic", "Legendary"]
+        rarity = random.choices(rarities, weights=[70, 20, 8, 2])[0]
+        
+        nft = {
+            'id': nft_id,
+            'owner': user_id,
+            'name': f"NFT #{nft_id}",
+            'rarity': rarity,
+            'value': random.randint(100, 5000),
+            'created': datetime.now()
+        }
+        
+        self.nft_collection[nft_id] = nft
+        return nft
     
-    async def create_auction(self, nft_id: str, start_price: int):
-        """⚡ Аукцион NFT"""
-        # Ставки в реальном времени
-        # Таймер обратного отсчета
-        # Уведомления участников
-        pass
-    
-    async def nft_battles(self, nft1: str, nft2: str):
-        """⚔️ Битвы NFT за награды"""
-        # PvP битвы
-        # Прокачка характеристик
-        # Рейтинговые сезоны
-        pass
+    async def get_user_nfts(self, user_id: int):
+        return [nft for nft in self.nft_collection.values() if nft['owner'] == user_id]
+
+nft_service = NFTService()

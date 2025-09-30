0c91dac8737469ca6644cd05636822a6b1303dcf
q1mlvd <mishakravhuk@gmail.com>
2025-09-30 20:59:14 +0200

    Update cache.py

--- utils/cache.py
+++ utils/cache.py
@@ -1,9 +1,26 @@
-class RedisCache:
+import time
+
+class Cache:
     def __init__(self):
-        pass
+        self.data = {}
+        self.expire_times = {}
     
-    async def set_cache(self, key, value):
-        pass
+    def set(self, key: str, value, expire: int = 3600):
+        self.data[key] = value
+        self.expire_times[key] = time.time() + expire
     
-    async def get_cache(self, key):
-        return None
+    def get(self, key: str):
+        if key in self.data and time.time() < self.expire_times.get(key, 0):
+            return self.data[key]
+        else:
+            if key in self.data:
+                del self.data[key]
+                del self.expire_times[key]
+            return None
+    
+    def delete(self, key: str):
+        if key in self.data:
+            del self.data[key]
+            del self.expire_times[key]
+
+cache = Cache()

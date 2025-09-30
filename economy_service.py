b03f17841773f2a9423c54bb58eb0b4b45964db7
q1mlvd <mishakravhuk@gmail.com>
2025-09-30 20:05:09 +0200

    Create economy_service.py

---
+++ economy_service.py
@@ -0,0 +1,26 @@
+import discord
+import asyncio
+import random
+from datetime import datetime, timedelta
+
+class EconomyService:
+    def __init__(self, bot):
+        self.bot = bot
+        self.user_investments = {}
+        self.user_insurance = {}
+        self.user_lottery_tickets = {}
+    
+    async def start_money_rain(self, duration: int):
+        """💰 Денежный дождь - х2 награды"""
+        # Код ивента
+        pass
+    
+    async def create_stock_market(self):
+        """📈 Фондовый рынок с акциями"""
+        # Реальные котировки
+        pass
+    
+    async def start_battle_royale(self):
+        """🎮 Экономическая битва на выживание"""
+        # 100 участников, 1 победитель
+        pass
